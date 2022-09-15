e04783f9a94897f4ef07652f9e68b148cbd104a2
kumbulali <alikumbul@windowslive.com>
2022-09-15 10:58:11 +0300

    last commit before testing

--- src/middlewares/checkJwt.ts
+++ src/middlewares/checkJwt.ts
@@ -3,18 +3,15 @@ import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
 
 export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
-  //Get the jwt token from the head
   const token = <string>req.headers["authorization"];
 
   let jwtPayload;
 
-  //Try to validate the token and get data
   try {
     const bearerToken = token.slice(7);
     jwtPayload = <any>jwt.verify(bearerToken, config.jwtSecret);
     res.locals.jwtPayload = jwtPayload;
   } catch (error) {
-    //If token is not valid, respond with 401 (unauthorized)
     res.status(401).json({
       success: 0,
       message: "Unauthorized user.",
@@ -28,6 +25,5 @@ export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
   });
   res.setHeader("token", newToken);
 
-  //Call the next middleware or controller
   next();
 };
